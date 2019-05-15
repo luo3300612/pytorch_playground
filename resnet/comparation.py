c7b67a2f3a368d655a76fa130d3dd713a4af5261
luo3300612 <591486669@qq.com>
2019-05-15 21:32:47 +0000

    update

--- resnet/comparation.py
+++ resnet/comparation.py
@@ -1,9 +1,12 @@
 from torchvision.models.resnet import resnet18
 import torch
+from main import ResNet18_new
 
 config = {"num_classes": 10}
 
-net = resnet18(**config)
+# net = resnet18(**config)
+
+net = ResNet18_new(3,10)
 
 x = torch.randn(size=[5, 3, 224, 64])
 
--- resnet/main.py
+++ resnet/main.py
@@ -17,19 +17,21 @@ device = torch.device("cpu")
 class ResNet18(nn.Module):
     def __init__(self, H, W, in_channel=3, num_classes=10):
         super(ResNet18, self).__init__()
-        self.conv1 = nn.Conv2d(in_channel, 64, kernel_size=7, stride=2, padding=3)
-        # self.bn1 = nn.BatchNorm2d(64)
+        self.conv1 = nn.Conv2d(in_channel, 64, kernel_size=7, stride=2, padding=3, bias=False)
+        self.bn1 = nn.BatchNorm2d(64)
+        self.relu = nn.ReLU(inplace=True)
         self.maxpool = nn.MaxPool2d(2, 2)
         self.block1 = ResNetBlock(64, 64, 2, down_sample=False)
         self.block2 = ResNetBlock(64, 128, 2)
         self.block3 = ResNetBlock(128, 256, 2)
         self.block4 = ResNetBlock(256, 512, 2)
-        self.avgpool = nn.AvgPool2d((H // (2 ** 5), W // (2 ** 5)))  # self.avgpool = nn.AdaptiveAvgPool2d((1, 1))
+        self.avgpool = nn.AdaptiveAvgPool2d((1, 1))  # self.avgpool = nn.AdaptiveAvgPool2d((1, 1))
         self.flatten = Flatten()
         self.fc = nn.Linear(512, num_classes)
 
     def forward(self, x):
         x = self.conv1(x)
+        x = self.relu(x)
         x = self.maxpool(x)
         x = self.block1(x)
         x = self.block2(x)
@@ -67,6 +69,85 @@ class ResNet34(nn.Module):
         return x
 
 
+class ResNet18_new(nn.Module):
+    """
+    后面有bn bias可以是Fasle
+    nn.AdaptiveAvgPool2d好用
+    nn.Sequential好用
+    """
+
+    def __init__(self, in_channel, num_classes=1000):
+        super(ResNet18_new, self).__init__()
+        self.conv1 = nn.Conv2d(in_channel, 64, kernel_size=7, stride=2, padding=3, bias=False)
+        self.bn1 = nn.BatchNorm2d(64)
+        self.relu = nn.ReLU(inplace=True)
+        self.maxpool = nn.MaxPool2d(2, 2)
+        self.block1 = self._make_layer(64, 64, 3)
+        self.block2 = self._make_layer(64, 128, 3, downsample=True)
+        self.block3 = self._make_layer(128, 256, 6, downsample=True)
+        self.block4 = self._make_layer(256, 512, 3, downsample=True)
+        self.avgpool = nn.AdaptiveAvgPool2d((1, 1))
+        self.fc = nn.Linear(512, num_classes)
+
+    def _make_layer(self, in_channel, out_channel, num_block, downsample=False):
+        layers = []
+        if downsample:
+            downsample = nn.Conv2d(in_channel, out_channel, kernel_size=1, stride=2)
+        else:
+            downsample = None
+
+        layers.append(ResNetBlock_new(in_channel, out_channel, downsample))
+        for i in range(1, num_block):
+            layers.append(ResNetBlock_new(out_channel, out_channel))
+
+        return nn.Sequential(*layers)
+
+    def forward(self, x):
+        x = self.conv1(x)
+        x = self.bn1(x)
+        x = self.relu(x)
+        x = self.maxpool(x)
+        x = self.block1(x)
+        x = self.block2(x)
+        x = self.block3(x)
+        x = self.block4(x)
+        x = self.avgpool(x)
+        x = x.view((x.shape[0],-1))
+        x = self.fc(x)
+        return x
+
+
+class ResNetBlock_new(nn.Module):
+    def __init__(self, in_channel, out_channel, downsample=None):
+        super(ResNetBlock_new, self).__init__()
+        self.in_channel = in_channel
+        self.out_channel = out_channel
+        self.downsample = downsample
+        if downsample is None:
+            self.conv1 = nn.Conv2d(in_channel, out_channel, kernel_size=3,
+                                   stride=1, padding=1, bias=False)
+        else:
+            self.conv1 = nn.Conv2d(in_channel, out_channel, kernel_size=3,
+                                   stride=2, padding=1, bias=False)
+        self.bn1 = nn.BatchNorm2d(out_channel)
+        self.relu = nn.ReLU(inplace=False)
+        self.conv2 = nn.Conv2d(out_channel, out_channel, kernel_size=3,
+                               stride=1, padding=1, bias=False)
+        self.bn2 = nn.BatchNorm2d(out_channel)
+
+    def forward(self, x):
+        out = self.conv1(x)
+        out = self.bn1(out)
+        out = self.relu(out)
+        out = self.conv2(out)
+        if self.downsample is not None:
+            x = self.downsample(x)
+        out = out + x  # TODO together batch normalization??
+        out = self.bn2(out)
+        out = self.relu(out)
+        return out
+
+
 class ResNetBlock(nn.Module):
     def __init__(self, in_channel, out_channel, block_num, down_sample=True):
         super(ResNetBlock, self).__init__()
@@ -75,9 +156,16 @@ class ResNetBlock(nn.Module):
         self.block_num = block_num
         self.down_sample = down_sample
 
+        self.convx = None
+        if down_sample:
+            self.convx = nn.Conv2d(in_channel, out_channel, kernel_size=1, stride=2, bias=False)
+
         self.relu = nn.ReLU()
         self.maxpool = nn.MaxPool2d(2, 2)
-        for i in range(self.block_num):
+        self.layers = []
+        self.layers.append(nn.Conv2d(in_channel, out_channel, kernel_size=3, stride=1 + int(down_sample),
+                                     padding=1))
+        for i in range(1, self.block_num):
             if i == 0:
                 self.__setattr__(f"conv{i + 1}_1",
                                  nn.Conv2d(in_channel, out_channel, kernel_size=3, stride=1 + int(down_sample),
@@ -114,14 +202,14 @@ class Flatten(nn.Module):
         return x.view(x.shape[0], -1)
 
 
-def adjust_learning_rate(optimizer, iteration,init_lr=0.1):
+def adjust_learning_rate(optimizer, iteration, init_lr=0.1):
     """Sets the learning rate to the initial LR decayed by 10 every 30 epochs"""
     if iteration <= 32000:
         lr = init_lr
     elif 32000 < iteration <= 48000:
-        lr = init_lr/10
+        lr = init_lr / 10
     else:
-        lr = init_lr/100
+        lr = init_lr / 100
     for param_group in optimizer.param_groups:
         param_group['lr'] = lr
     return lr
