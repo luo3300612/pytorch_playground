b9f58f5e26aed72e18f75e374644775175b2f420
luo3300612 <591486669@qq.com>
2019-05-16 14:19:42 +0000

    update

--- resnet/main.py
+++ resnet/main.py
@@ -292,14 +292,14 @@ if __name__ == '__main__':
                                                   transforms.RandomCrop(32, 4),
                                                   transforms.RandomHorizontalFlip(),
                                                   transforms.ToTensor(),
-                                                  transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)),
+                                                  transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
                                               ]),
                                               download=True)
     test_data = torchvision.datasets.CIFAR10(root='./data/',
                                              train=False,
                                              transform=transforms.Compose([
                                                  transforms.ToTensor(),
-                                                 transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
+                                                 transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010))
                                              ]),
                                              download=True)
 
